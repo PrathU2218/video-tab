d748f5e0fe09622cc2909cc1249bad63230cec97
Prathmesh Namdeo <prathmesh.namdeo22@gmail.com>
2020-05-29 20:30:26 +0530

    commit by Prathmesh

--- src/app/video-tab/video-tab.component.ts
+++ src/app/video-tab/video-tab.component.ts
@@ -11,41 +11,66 @@ export class VideoTabComponent implements OnInit {
     video: "video.mp4",
     title: "title",
     upvotes: 1400,
+    downvotes: 3,
     upnext: [
       {title: "title",
     upvotes: 2000,
+    downvotes: 34,
     video: null,
     creator: "danish",
     references: null,
     comments: null,
     upnext: null,
-    thumbnail: ""
+    thumbnail: "assets/images/user-1.jpg"
   },
   {title: "title",
   upvotes: 2000,
+  downvotes: 4,
   video: null,
   creator: "danish",
   references: null,
   comments: null,
   upnext: null,
-  thumbnail: ""
+  thumbnail: "assets/images/user-1.jpg"
 },
 {title: "title",
 upvotes: 2000,
+downvotes: 37,
 video: null,
 creator: "danish",
 references: null,
 comments: null,
 upnext: null,
-thumbnail: ""
-}
+thumbnail: "assets/images/user-1.jpg"
+},
+
+{title: "title",
+upvotes: 2000,
+downvotes: 34,
+video: null,
+creator: "danish",
+references: null,
+comments: null,
+upnext: null,
+thumbnail: "assets/images/user-1.jpg"
+},
+{title: "title",
+upvotes: 2000,
+downvotes: 34,
+video: null,
+creator: "danish",
+references: null,
+comments: null,
+upnext: null,
+thumbnail: "assets/images/user-1.jpg"
+},
     ],
     creator: "danish",
     references:[
       "1st Reference",
       "2nd reference"
     ],
-    thumbnail: "",
+    thumbnail: "assets/images/user-1.jpg",
     comments: [
       {poster: "danish",
       comment: "this is the first comment",
@@ -75,6 +100,7 @@ class Video{
   video: String;
   title: String;
   upvotes: number;
+  downvotes: number;
   upnext: Video[];
   creator: String;
   references: String[];
